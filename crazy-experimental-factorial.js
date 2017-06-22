985748fa400e95c76669115d9f0bf17fadb789ce
Paul Gowder <paul.gowder@gmail.com>
2017-06-22 00:47:17 -0500

    another experimental factorial that achieves memoization of intermediate results at cost of double calculation the first time.  maybe worth it for some use cases

--- crazy-experimental-factorial.js
+++ crazy-experimental-factorial.js
@@ -1,33 +1,37 @@
-// experimental hand-rolled memoization to keep inner values
-
 "use strict";
-var count = 0
 var cache = new Map();
 cache.set(0,0);
 cache.set(1,1);
 
+// this is even more aggressively memoized than before.  will take twice the time on the first run, but thereafter should get all intermediate results instantly.  Should build this into other module as an aggressive option, for when lots of factorials need to be called and not necessarily all in order.  This should optimize that like woah.
+
+const windDown = function (num, result){
+    if (cache.get(num)){
+        return null;
+    }
+    cache.set(num, result);
+    windDown(num - 1, result / num)
+}
+
 const innerFact = function(num, orignum, acc){
-    count += 1;
     if (cache.get(num)) {
-        console.log("done in: ", count, " steps. ")
         var result = cache.get(num) * acc;
-        cache.set(orignum, result);
+        windDown(orignum, result);
         return result
     }
     return innerFact(num - 1, orignum, num * acc);
 };
 
 function factorial(num) {
-    count = 0;
     return innerFact(num, num, 1);
 }
 
+console.log(factorial(5));
+console.log(cache);
+console.log(factorial(4));
+console.log(cache);
+console.log(factorial(6));
+console.log(cache);
+
 module.exports = {factorial};
 
-// the problem with getting truly intermediate values is I never have them.  factorial(4) never calculates factorial(3), for example.  It would be nice if it did, but it doesn't.  it calculates a different function of 3.
-
-// but if I walked UPWARD the numbers from 1 or 2 rather than downward from the big nums...
-
-// but that would be dumb, because then if I calculated fact(10,000,000) I'd have to do a hash lookup for everything even if I'd previously done fact(9,999,999) and that would be ridiculously inefficient.  I'd optimize the bigfact->smallfact path at the cost of de-optimizing the smallfact->bigfact path.
-
-// still, I'm happy here, I've beat ramda's memoization.
