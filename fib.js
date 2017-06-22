2f9b0a77fde7295a441a7079fbe5b44edcd75410
Paul Gowder <paul.gowder@gmail.com>
2017-06-22 17:57:10 -0500

    add a clojurey fib

--- fib.js
+++ fib.js
@@ -1,1 +1,14 @@
-//TODO
+"use strict";
+
+const M = require('mori')
+
+function fiboHelper([a, b]) {
+    return [b, a + b];
+}
+
+function fibonacci(n){
+    const lazyFibs = M.map(M.first, M.iterate(fiboHelper, [0, 1]));
+    return M.take(n, lazyFibs);
+}
+
+console.log(fibonacci(20))
