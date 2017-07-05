b8f88ecc7dbadf690603f3fb53497bb0d2867220
Paul Gowder <paul.gowder@gmail.com>
2017-07-05 15:51:20 -0400

    monads

--- haskell/fib.hs
+++ haskell/fib.hs
@@ -1,12 +1,20 @@
-fib :: Int -> Int
-fib 0 = 0
-fib 1 = 1
-fib x | x < 0 = 0
-fib x = fib (x - 1) + fib (x - 2)
+yankiepoo :: Maybe Int -> Int
+yankiepoo Nothing = 0
+yankiepoo (Just x) = x
+
+fib :: Int -> Maybe Int
+fib 0 = Just 0
+fib 1 = Just 1
+fib x | x < 0 = Nothing
+fib x = Just (yankiepoo (fib (x - 1)) + yankiepoo (fib (x - 2)))
+
+-- this isn't tail recursive... is that a problem?
 
 fiblist :: Int -> [Int]
-fiblist x = take x $ map fib [0..]
+fiblist x =
+  let maybelist = map fib [0..]
+      intlist = map yankiepoo maybelist
+  in take x intlist
 
--- probably ought to have a maybe monad or something to get a Nothing in as result of negative fib?
 
 -- fiblist (-2) -> [] --- which is fine, but I don't understand why it gets there. what does range operator do when x is out of range?
